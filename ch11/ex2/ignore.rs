511550bfa944937258957a8affa560b75c61495d
kuwana-kb <41671293+euryalis@users.noreply.github.com>
2019-08-21 02:26:08 +0900

    add chapter 11

---
+++ ch11/ex2/ignore.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        
+    }
+}
